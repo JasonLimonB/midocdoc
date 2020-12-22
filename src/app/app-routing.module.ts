017dcc81a236a81dc3000f8d3e12a6c83806943d
Jason <jason.limon@outlook.com>
2020-12-21 22:37:00 -0600

    Creando bas de proyecto 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LoginPageComponent
   ]
 })
 export class AuthModule { }
